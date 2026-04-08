7c9e1f53c3104a8d03bc1fc130033154730955f1
Oumuamua <praveenpanangadan07@gmail.com>
2026-04-09 00:20:58 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// We removed StrictMode to prevent double-loading of 3D assets & audio
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App />
 )
